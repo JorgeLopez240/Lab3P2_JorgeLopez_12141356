8e888fefc6ff1b28ca8e5d4b771a6aec4a46f5d2
unknown <jflg240104@unitec.edu>
2022-08-05 13:14:02 -0600

    Se agrego el proyecto

---
+++ src/lab3p2_jorgelopez_12141356/Lab3P2_JorgeLopez_12141356.java
@@ -0,0 +1,15 @@
+package lab3p2_jorgelopez_12141356;
+
+import java.util.Scanner;
+
+public class Lab3P2_JorgeLopez_12141356 {
+
+    static Scanner lea = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+}
