3a9edc77d921c29f4968dd7bbe4464db73132a58
unknown <jflg240104@unitec.edu>
2022-08-05 13:39:36 -0600

    termine el uml

--- src/lab3p2_jorgelopez_12141356/Lab3P2_JorgeLopez_12141356.java
+++ src/lab3p2_jorgelopez_12141356/Lab3P2_JorgeLopez_12141356.java
@@ -8,8 +8,55 @@ public class Lab3P2_JorgeLopez_12141356 {
     
     public static void main(String[] args) {
         
-        
-        
+        boolean flag = true;
+        while(flag == true){
+            System.out.println();
+            System.out.println("0. Salir");
+            System.out.println("1. Crear Cohete");
+            System.out.println("2. Crear Planeta");
+            System.out.println("3. Editar Cohete");
+            System.out.println("4. Editar Planeta");
+            System.out.println("5. Listar Cohetes");
+            System.out.println("6. Listar Planetas");
+            System.out.println("7. Probar Cohete");
+            System.out.println("Ingrese la opción: ");
+            int op = lea.nextInt();
+            System.out.println();
+            switch(op){
+                case 0:{
+                    flag=false;
+                }
+                break;
+                case 1:{
+                    
+                }
+                break;
+                case 2:{
+                    
+                }
+                break;
+                case 3:{
+                    
+                }
+                break;
+                case 4:{
+                    
+                }
+                break;
+                case 5:{
+                    
+                }
+                break;
+                case 6:{
+                    
+                }
+                break;
+                case 7:{
+                    
+                }
+                break;
+            }
+        }
     }
     
 }
